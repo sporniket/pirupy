5016cafb7dc2d6fbc364bf20c8848cead8b646f1
David SPORN <sporniket.studio@gmail.com>
2022-05-08 10:46:10 +0200

    prepare __all__

---
+++ src/pirupy/__init__.py
@@ -0,0 +1,25 @@
+"""
+---
+(c) 2022 David SPORN
+---
+This file is part of PIRUPY.
+
+PIRUPY is free software: you can redistribute it and/or modify it under the terms of
+the GNU General Public License as published by the Free Software Foundation, either
+version 3 of the License, or (at your option) any later version.
+
+PIRUPY is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with PIRUPY.
+If not, see <https://www.gnu.org/licenses/>. 
+---
+"""
+from .piru import *
+
+__all__ = [
+    pipeline, job,
+    before_all, after_all,
+    before_each, after_each
+]
